6822e8cc5314fc6c8dc218401aa8a1b32a1c8dfe
r3back <andresponce1334@gmail.com>
2023-06-14 16:13:15 -0300

    Se cambia el nombre del modulo

---
+++ Bakery-Project/Application.cs
@@ -0,0 +1,21 @@
+﻿using Application.fabrica.impl;
+using Application.servicio;
+using Application.util;
+
+namespace Application;
+
+public class Application
+{
+    private static readonly IServicioPasteleria Pasteleria = new FabricaPasteleria().CrearServicioPasteleria();
+    
+    public static void Main(string[] args)
+    {
+        MenuUtil.MenuPrincipal(Pasteleria);
+    }
+
+    // Singleton
+    public static IServicioPasteleria ObtenerInstancia()
+    {
+        return Pasteleria;
+    }
+}
